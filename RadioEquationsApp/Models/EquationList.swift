5b31adb8eeacdf987b3af965e11314f5e5a6e7fb
Robby Sarvis <rjs.app.dev@gmail.com>
2023-07-15 23:12:58 -0500

    add chatgpt generated equation descriptions

--- RadioEquationsApp/Models/EquationList.swift
+++ RadioEquationsApp/Models/EquationList.swift
@@ -12,28 +12,87 @@ struct EquationsTableSectionModel {
     let equations: [Equation]
 }
 
+let VoltageCurrentResistanceDescriptionString = """
+The equation E = I x R represents the relationship between electrical energy (E), current (I), and resistance (R) in an electrical circuit.
+
+Here's a breakdown of each component:
+
+E represents electrical energy and is measured in joules (J) or sometimes in electron volts (eV). It refers to the amount of energy transferred or used by an electrical circuit or device.
+I stands for current and is measured in amperes (A). Current refers to the flow of electric charge in a circuit, specifically the rate at which charges move through a conductor.
+R represents resistance and is measured in ohms (Ω). Resistance is a property of a circuit element, such as a resistor or a wire, that opposes the flow of electric current.
+According to Ohm's Law, the equation E = I x R expresses the relationship between these three variables. It states that the electrical energy (E) dissipated or consumed in a circuit is equal to the product of the current (I) flowing through the circuit and the resistance (R) encountered by the current.
+
+In practical terms, this equation can be used to calculate the energy consumption or dissipation in a circuit when the current and resistance values are known. Alternatively, it can be rearranged to solve for other variables. For example, if you know the energy and resistance, you can calculate the current by rearranging the equation as I = E / R.
+"""
+
+let PowerVoltageCurrentDescriptionString = """
+The equation P = E x I represents the relationship between power (P), electrical energy (E), and current (I) in an electrical circuit.
+
+Here's a breakdown of each component:
+
+P represents power and is measured in watts (W). Power refers to the rate at which energy is transferred, used, or consumed in a circuit or device.
+E represents electrical energy and is measured in joules (J) or sometimes in electron volts (eV). It refers to the amount of energy transferred or used by an electrical circuit or device.
+I stands for current and is measured in amperes (A). Current refers to the flow of electric charge in a circuit, specifically the rate at which charges move through a conductor.
+The equation P = E x I expresses the relationship between these variables. It states that the power (P) in a circuit is equal to the product of the electrical energy (E) and the current (I) flowing through the circuit.
+
+This equation is derived from the definition of power, which is the rate at which energy is transferred. By multiplying the energy transferred (E) by the rate at which it is transferred (I), we obtain the power.
+
+In practical terms, this equation is commonly used to calculate the power consumption or dissipation in a circuit when the energy and current values are known. It can also be rearranged to solve for other variables. For example, if you know the power and current, you can calculate the energy by rearranging the equation as E = P / I.
+"""
+
+let PowerCurrentResistanceDescriptionString = """
+The equation P = I^2 x R represents the relationship between power (P), current (I), and resistance (R) in an electrical circuit.
+
+Here's a breakdown of each component:
+
+P represents power and is measured in watts (W). Power refers to the rate at which energy is transferred, used, or consumed in a circuit or device.
+I stands for current and is measured in amperes (A). Current refers to the flow of electric charge in a circuit, specifically the rate at which charges move through a conductor.
+R represents resistance and is measured in ohms (Ω). Resistance is a property of a circuit element, such as a resistor or a wire, that opposes the flow of electric current.
+The equation P = I^2 x R expresses the relationship between these variables. It states that the power (P) in a circuit is equal to the square of the current (I) multiplied by the resistance (R) encountered by the current.
+
+This equation is derived from Ohm's Law, which states that the current flowing through a conductor is directly proportional to the voltage across it and inversely proportional to the resistance. By substituting the expression for current from Ohm's Law (I = V / R) into the equation P = I^2 x R, we can derive the power formula.
+
+In practical terms, this equation is commonly used to calculate the power dissipation in a circuit when the current and resistance values are known. It illustrates that power is directly proportional to the square of the current and the resistance. Therefore, a circuit with higher current or higher resistance will dissipate more power. Conversely, decreasing either the current or resistance will result in lower power dissipation.
+"""
+
+let PowerVoltageResistanceDescriptionString = """
+The equation P = E^2 / R represents the relationship between power (P), electrical energy (E), and resistance (R) in an electrical circuit.
+
+Here's a breakdown of each component:
+
+P represents power and is measured in watts (W). Power refers to the rate at which energy is transferred, used, or consumed in a circuit or device.
+E represents electrical energy and is measured in joules (J) or sometimes in electron volts (eV). It refers to the amount of energy transferred or used by an electrical circuit or device.
+R represents resistance and is measured in ohms (Ω). Resistance is a property of a circuit element, such as a resistor or a wire, that opposes the flow of electric current.
+The equation P = E^2 / R expresses the relationship between these variables. It states that the power (P) in a circuit is equal to the square of the electrical energy (E) divided by the resistance (R) encountered by the current.
+
+This equation can be derived by combining Ohm's Law (V = I x R) with the power formula (P = V x I). Since voltage (V) can be expressed as V = E / Q (where Q is the charge), and current (I) can be expressed as I = Q / t (where t is time), we can substitute these expressions into the power formula and rearrange to obtain P = E^2 / R.
+
+In practical terms, this equation is commonly used to calculate the power dissipation in a circuit when the electrical energy and resistance values are known. It shows that power is inversely proportional to the resistance and that doubling the resistance will halve the power dissipation. Similarly, increasing the electrical energy will result in higher power, assuming the resistance remains constant.
+"""
+
+
 let EquationsTableInfo = [
     EquationsTableSectionModel(title: "Voltage", equations: [
-        Equation(title: "[math]$E = I \\times R$[/math]", description: "Calculate voltage in a circuit", id: .voltage1),
-        Equation(title: "[math]$E = P / I$[/math]", description: "Calculate voltage in a circuit", id: .voltage2),
-        Equation(title: "[math]$E = \\sqrt{P \\times R}$[/math]", description: "Calculate voltage in a circuit", id: .voltage3)
+        Equation(title: "[math]$E = I \\times R$[/math]", description: VoltageCurrentResistanceDescriptionString, id: .voltage1),
+        Equation(title: "[math]$E = P / I$[/math]", description: PowerVoltageCurrentDescriptionString, id: .voltage2),
+        Equation(title: "[math]$E = \\sqrt{P \\times R}$[/math]", description: PowerVoltageResistanceDescriptionString, id: .voltage3)
     ]),
     
     EquationsTableSectionModel(title: "Resistance", equations: [
-        Equation(title: "[math]$R = E / I$[/math]", description: "Calculate resistance in a circuit.", id: .resistance1),
-        Equation(title: "[math]$R = P / I^2$[/math]", description: "Calculate resistance in a circuit.", id: .resistance2),
-        Equation(title: "[math]$R = E^2 / P $[/math]", description: "Calculate resistance in a circuit.", id: .resistance3)
+        Equation(title: "[math]$R = E / I$[/math]", description: VoltageCurrentResistanceDescriptionString, id: .resistance1),
+        Equation(title: "[math]$R = P / I^2$[/math]", description: PowerCurrentResistanceDescriptionString, id: .resistance2),
+        Equation(title: "[math]$R = E^2 / P $[/math]", description: PowerVoltageResistanceDescriptionString, id: .resistance3)
     ]),
     
     EquationsTableSectionModel(title: "Current", equations: [
-        Equation(title: "[math]$I = E / R$[/math]", description: "Calculate current in a circuit.", id: .current1),
-        Equation(title: "[math]$I = P / E$[/math]", description: "Calculate current in a circuit.", id: .current2),
-        Equation(title: "[math]$I = \\sqrt{P / R}$[/math]", description: "Calculate current in a circuit.", id: .current3)
+        Equation(title: "[math]$I = E / R$[/math]", description: VoltageCurrentResistanceDescriptionString, id: .current1),
+        Equation(title: "[math]$I = P / E$[/math]", description: PowerVoltageCurrentDescriptionString, id: .current2),
+        Equation(title: "[math]$I = \\sqrt{P / R}$[/math]", description: PowerCurrentResistanceDescriptionString, id: .current3)
     ]),
     
     EquationsTableSectionModel(title: "Power", equations: [
-        Equation(title: "[math]$P = E \\times I$[/math]", description: "Calculate power in a circuit.", id: .power1),
-        Equation(title: "[math]$P = E^2 / R$[/math]", description: "Calculate power in a circuit.", id: .power2),
-        Equation(title: "[math]$P = I^2 \\times R$[/math]", description: "Calculate power in a circuit.", id: .power3)
+        Equation(title: "[math]$P = E \\times I$[/math]", description: PowerVoltageCurrentDescriptionString, id: .power1),
+        Equation(title: "[math]$P = E^2 / R$[/math]", description: PowerVoltageResistanceDescriptionString, id: .power2),
+        Equation(title: "[math]$P = I^2 \\times R$[/math]", description: PowerCurrentResistanceDescriptionString, id: .power3)
     ])
 ]
