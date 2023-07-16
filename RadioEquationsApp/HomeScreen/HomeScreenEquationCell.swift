4446d4d130e6e05529a0973d3803210e88cdd0ba
Robby Sarvis <rjs.app.dev@gmail.com>
2023-07-15 23:01:29 -0500

    tweak horizontal alignment for table rows

--- RadioEquationsApp/HomeScreen/HomeScreenEquationCell.swift
+++ RadioEquationsApp/HomeScreen/HomeScreenEquationCell.swift
@@ -29,6 +29,7 @@ class HomeScreenEquationCell: UITableViewCell {
         
         addSubview(equationLabel)
         equationLabel.centerY(inView: self)
+        equationLabel.anchor(left: self.leftAnchor, paddingLeft: 32)
     }
     
     required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
